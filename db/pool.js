243c20e2aa0dc595a5fabcde82fffa3f6d6fccd1
Juan Ayala <zonadetrabajo2017@gmail.com>
2025-12-15 03:09:29 -0300

    Fix SSL connection for Railway Postgres

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,11 @@
 const { Pool } = require("pg");
 
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: {
+    require: true,
     rejectUnauthorized: false
   }
 });
